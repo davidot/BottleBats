700bdf2b05464fa1946fd8a4e5f63564e88461fc
davidot <david.tuin@gmail.com>
2022-04-04 01:37:27 +0200

    Site: Start specific vijf leaderboard items

--- site/src/components/Leaderboard.vue
+++ site/src/components/Leaderboard.vue
@@ -10,11 +10,7 @@
       <div style="height: 500px; width: 500px; overflow-y: scroll; overflow-x: hidden">
         <div style="width: 80%; margin: auto; display: flex; flex-direction: column">
           <TransitionGroup name="alist" move-class="moving-item">
-
-            <component :is="'div'" v-for="(nm, index) in things" :key="nm" class="anim-item" :data-change="index <= (oldIndices[index] || -1) ? 'pos' : 'neg'">
-              <span class="name">
-                {{ nm.nm }}
-              </span>
+            <component :is="'vijf-leaderboard-item'" v-for="(item, index) in orderedItems" :key="'lb-' + item.itemId" :data="item" class="anim-item" :data-change="index <= (oldIndices[index] || -1) ? 'positive-change' : 'negative-change'">
               {{ index }} vs {{ oldIndices[index] || -1 }}
             </component>
           </TransitionGroup>
@@ -26,21 +22,31 @@
 
 <script>
 
+import VijfLeaderboardItem from "@/components/vijf/VijfLeaderboardItem.vue";
+
 export default {
   name: "LeaderBoard",
+  components: {
+    VijfLeaderboardItem,
+  },
   data() {
     return {
       count: 1,
-      things: [{ nm: "hi" }, {nm: "hello"}, {nm: "bye"}],
+      things: [{ rank: 2, itemId: '123y5d', name: "hi" }, {rank: 0, itemId: '123152by5d',name: "hello"}, {rank: 1, itemId: 'asdg80021',name: "bye"}],
       oldIndices: [],
     };
+  },
+  computed: {
+    orderedItems() {
+      return [...this.things].sort((lhs, rhs) => lhs.rank - rhs.rank);
+    },
   },
   methods: {
     fillOldIndices() {
       this.oldIndices = [...this.things.keys()];
     },
     addMe() {
-      this.things.push({nm: this.things[this.things.length - 1].nm + 'b'});
+      // this.things.push({nm: this.things[this.things.length - 1].nm + 'b'});
 
       this.fillOldIndices();
     },
@@ -53,17 +59,22 @@ export default {
     shuffle() {
       this.fillOldIndices();
 
+      const indices = [...this.things.keys()];
+
       for (let i = 0; i < this.things.length; i++) {
         let number = Math.floor(i + (this.things.length - i) * Math.random());
-        const temp = this.things[i];
-        this.things[i] = this.things[number];
-        this.things[number] = temp;
+        const temp = indices[i];
+        indices[i] = indices[number];
+        indices[number] = temp;
+      }
 
-        const tempIdx = this.oldIndices[i];
-        this.oldIndices[i] = this.oldIndices[number];
-        this.oldIndices[number] = tempIdx;
+      for (let i = 0; i < this.things.length; i++) {
+        // const tempIdx = this.oldIndices[i];
+        // this.oldIndices[i] = this.oldIndices[number];
+        // this.oldIndices[number] = tempIdx;
 
       }
+
     },
   },
 };
@@ -102,7 +113,7 @@ export default {
   /*transform: translateY(50px);*/
 }
 
-.moving-item[data-change="neg"] {
+.moving-item[data-change="negative-change"] {
   color: red;
 }
 
@@ -115,10 +126,10 @@ export default {
   transform: scale(150%);
 }
 
-.anim-item:first-child {
-  transform: scale(125%) translateY(25%);
-  transform-origin: 0 0;
-  margin-bottom: 10px;
-}
+/*.anim-item:first-child {*/
+/*  transform: scale(125%) translateY(25%);*/
+/*  transform-origin: 0 0;*/
+/*  margin-bottom: 10px;*/
+/*}*/
 
 </style>
---
+++ site/src/components/vijf/VijfLeaderboardItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="vijf-leaderboard-item">
+    {{ data.name }} |
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VijfLeaderboardItem",
+  props: {
+    data: {},
+  },
+};
+</script>
+
+<style scoped>
+
+.vijf-leaderboard-item {
+
+}
+
+</style>
