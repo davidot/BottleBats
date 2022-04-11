336b0db83fd2c27e455233313bd557a4528c8195
davidot <david.tuin@gmail.com>
2022-04-11 11:06:34 +0200

    Server: Add convenience function for running one thing against the db

--- server/database/ConnectionPool.h
+++ server/database/ConnectionPool.h
@@ -2,6 +2,7 @@
 
 #include <memory>
 #include <pqxx/connection>
+#include "../../util/Deferred.h"
 
 namespace BBServer::ConnectionPool {
 
@@ -10,5 +11,17 @@ void initialize_pool(uint32_t size, std::string const& connection_string);
 std::unique_ptr<pqxx::connection> get_connection();
 
 void return_connection(std::unique_ptr<pqxx::connection>&&);
+
+template<typename Function>
+auto run_on_temporary_connection(Function function)
+{
+    std::unique_ptr<pqxx::connection> connection = get_connection();
+    Deferred relase ([&]{
+        return_connection(std::move(connection));
+    });
+
+    return function(*connection);
+}
+
 
 }
