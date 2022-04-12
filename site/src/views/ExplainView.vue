86ee6e0b41ae7fda0b8683c21db06cc9c3cb4c73
davidot <david.tuin@gmail.com>
2022-04-12 13:43:29 +0200

    Site: Expand explanations

--- site/src/views/ExplainView.vue
+++ site/src/views/ExplainView.vue
@@ -5,16 +5,61 @@
       Vandaag spelen we <b>Vijf</b>.
     </p>
 
+    <h3>
+      Snelle linkies
+      <ul>
+        <li><a href="#explain-regels">Regels</a></li>
+        <li><a href="#explain-bot">Bot regels</a></li>
+        <li><a href="#explain-run">Input en output voorbeelden</a></li>
+        <li><a href="#explain-example">Code voorbeelden en tools</a></li>
+      </ul>
 
-    <h4>
+    </h3>
+
+
+    <h3 id="explain-regels">
       Regels
-    </h4>
-    <p>
-    </p>
+    </h3>
+    <div>
+      <h5>Je bent af als je een vijf krijgt.</h5>
 
-    <h4>
+
+      <h4>
+        Kaarten uitdelen
+      </h4>
+
+      Omstebeurt wordt aan iedereen drie kaarten uitgedeeld.
+      Zit er een vijf tussen de kaarten dan gaan de vijf(en) terug en krijg je nog zoveel kaarten.
+      Zit daar een vijf tussen? Helaas <b style="color: red">dood</b>.
+
+
+      <h4>
+        Kaarten spelen
+      </h4>
+      <p>Je bent in een rondje aan de beurt (als je nog niet af bent).
+      Je moet een kaart spelen, speel je alles behalve een regel kaart dan trek je zoveel kaarten als het getal op de kaart.
+        Met de speciale kaarten: Joker = 0, Aas = 1, Boer = 11, Vrouw = 12, Heer = 13.</p>
+
+      <p>Kom je een vijf tegen oepsie doepsie <b style="color: red">dood</b> en trek niet meer kaarten, volgende aan de beurt.
+      Kom je een regel kaart tegen, joepie stop met kaarten trekken.</p>
+
+      <p>Heb je net je laatste kaart gespeeld en nu een lege hand aan het einde van je beurt?
+      Hopelijk ben je als enige over want anders <b style="color: red">dood</b>.</p>
+
+      <h4>
+        Regelkaart
+      </h4>
+
+      <p>De regelkaart is speciaal bij het trekken van de kaart en het spelen.
+        Bij het trekken is het net zoals een 5 dat je moet stoppen. </p>
+
+      <p>Bij het spelen ga je in de spel richting alle nog spelers die nog niet af zijn langs en moeten ze twee kaarten trekken.
+        Zit er een vijf tussen helaas <b style="color: red">dood</b>. </p>
+    </div>
+
+    <h3 id="explain-bot">
       Bot regels
-    </h4>
+    </h3>
     <div>
       Als bot krijg je berichten via stdin (System.in, etc.). <br />
       En moet je antwoorden via stdout (System.out, print, etc.) <br />
@@ -64,9 +109,9 @@
     </div>
 
 
-    <h4>
+    <h3 id="explain-run">
       Input en output voorbeelden
-    </h4>
+    </h3>
     <div>
       Hier betekent <code> &gt; </code> van de runner naar de bot en <code> &lt; </code> een antwoord vanuit de bot
       <br>
@@ -109,17 +154,15 @@
     </div>
 
 
-    <h4>
-      Code voorbeelden
-    </h4>
+    <h3 id="explain-example">
+      Code voorbeelden en tools
+    </h3>
     <ul>
       <li><a href="examples/example.java" download>Java</a></li>
       <li><a href="examples/example.py" download>Python</a></li>
       <li><a href="examples/example.cpp" download>C++</a></li>
       <li><a href="examples/test-vijf.exe" download>Test programm</a></li>
     </ul>
-
-
   </div>
 </template>
 
