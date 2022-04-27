e7d6e12f95359d73eeb29955fee99189672328b9
davidot <david.tuin@gmail.com>
2022-04-28 01:37:00 +0200

    Example: Add C# example

---
+++ examples/cs/Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        string s = Console.ReadLine();
+        Console.WriteLine("ready: " + s);
+    }
+}
