b1236d7faa64060ce61f87393a0d49fd6d92e4a8
davidot <david.tuin@gmail.com>
2022-05-01 00:42:14 +0200

    Elevated: Fix PassengerStats and make it actually compile
    Also add getters to extract stats.

--- games/elevated/stats/PassengerStats.cpp
+++ games/elevated/stats/PassengerStats.cpp
@@ -18,19 +18,20 @@ void PassengerStatsListener::on_passenger_enter_elevator(Time at, Passenger cons
 
     Time wait_time = at - m_arrival_times[passenger.id];
     ++m_wait_times[wait_time];
+    m_max_wait_time = std::max(m_max_wait_time, wait_time);
     m_enter_times[passenger.id] = at;
     m_arrival_times.erase(passenger.id);
 }
 
 void PassengerStatsListener::on_passenger_leave_elevator(Time at, PassengerID id, Height)
 {
-    ASSERT(!m_arrival_times.contains(at));
-    ASSERT(m_enter_times.contains(at));
+    ASSERT(!m_arrival_times.contains(id));
+    ASSERT(m_enter_times.contains(id));
 
-    Time travel_time = at - m_arrival_times[id];
+    Time travel_time = at - m_enter_times[id];
     ++m_travel_times[travel_time];
-    m_enter_times[id] = at;
-    m_arrival_times.erase(id);
+    m_max_travel_time = std::max(m_max_travel_time, travel_time);
+    m_enter_times.erase(id);
 }
 
 }
--- games/elevated/stats/PassengerStats.h
+++ games/elevated/stats/PassengerStats.h
@@ -9,12 +9,16 @@ public:
     void on_passenger_enter_elevator(Time at, Passenger const& passenger, ElevatorID id) override;
     void on_passenger_leave_elevator(Time at, PassengerID id, Height height) override;
 
+    Time max_wait_times() const { return m_max_wait_time; }
+    Time max_travel_times() const { return m_max_travel_time; }
 private:
     std::unordered_map<PassengerID, Time> m_arrival_times;
     std::unordered_map<PassengerID, Time> m_enter_times;
 
     std::unordered_map<Time, uint64_t> m_wait_times;
     std::unordered_map<Time, uint64_t> m_travel_times;
+    Time m_max_wait_time{0};
+    Time m_max_travel_time{0};
 };
 
 
