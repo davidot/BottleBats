683d5a90118fe8a7ef652026c449fdfbc0cec19f
davidot <david.tuin@gmail.com>
2022-05-02 11:15:21 +0200

    Server: Add C# builder

---
+++ bots-scripts/cs/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+if [ $# -lt 2 ]; then
+ echo "no arguments"
+ exit 1
+fi
+
+filename="$1"
+container_name="$2"
+folder="$(dirname "$filename")"
+filename_only="$(basename "$filename")"
+
+pushd "$folder/" > /dev/null
+
+podman build -f /home/david/code/BottleBats/bots-scripts/cs/Dockerfile -t "$container_name" . --build-arg "filename=./$filename_only"
+
+popd > /dev/null
