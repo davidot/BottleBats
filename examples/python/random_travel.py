9b48884f8034a8dd63fb2f003e691302c6f28d70
davidot <david.tuin@gmail.com>
2022-05-02 10:48:51 +0200

    Examples: Add random algorithm

---
+++ examples/python/random_travel.py
@@ -0,0 +1,75 @@
+import random
+import sys
+
+verbose = False
+
+def pr(str):
+    if verbose:
+        sys.stderr.write(str + '\n')
+
+def read_line():
+    line = input()
+    if line == '':
+        pr('> [empty!]')
+    else:
+        pr('> ' + line)
+    return line
+
+def write_line(out):
+    pr('< ' + out)
+    print(out)
+
+next_line = read_line()
+
+floors = []
+
+while next_line != 'done':
+    next_line = read_line()
+
+    if next_line.startswith('building '):
+        parts = next_line.split(' ')
+        if int(parts[1]) > 1:
+            write_line('reject only work for single groups')
+            exit()
+        group = read_line()
+        group_parts = group.split(' ')
+        if len(group_parts) != 4 or not group.startswith('group '):
+            write_line('reject unexpected group line ' + group)
+            exit()
+
+        floors = [int(f) for f in group_parts[3].split(',')]
+
+
+pr('Got floors ' + str(floors))
+if len(floors) == 0:
+    write_line('reject empty group? ' + str(floors))
+    exit()
+
+rand = random.Random()
+
+
+def random_floor():
+    return str(floors[rand.randrange(0, len(floors))])
+
+write_line('ready')
+
+in_response = False
+
+while True:
+    next_line = read_line()
+
+    if next_line == 'stop':
+        break
+
+    if next_line.startswith('events 0'):
+        write_line('move 0 ' + random_floor())
+        write_line('set-timer 100')
+
+    if next_line == 'done':
+        write_line('done')
+        in_response = False
+    else:
+        parts = next_line.split(' ')
+        if not in_response:
+            write_line('move 0 ' + random_floor())
+        in_response = True
