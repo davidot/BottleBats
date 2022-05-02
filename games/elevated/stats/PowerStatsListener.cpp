a7325d4c128239322d632074fba3ef8edbfb8e1d
davidot <david.tuin@gmail.com>
2022-05-02 10:54:01 +0200

    Elevated: Add power stats listener

---
+++ games/elevated/stats/PowerStatsListener.cpp
@@ -0,0 +1,29 @@
+#include "PowerStatsListener.h"
+#include <numeric>
+
+namespace Elevated {
+
+void PowerStatsListener::on_elevator_opened_doors(Time, ElevatorState const&)
+{
+    ++m_times_door_opened;
+}
+
+void PowerStatsListener::on_elevator_moved(Time, Height height, ElevatorState const& state)
+{
+    m_distance_moved[state.filled_capacity()] += height;
+}
+
+size_t PowerStatsListener::total_distance_travelled() const
+{
+    return std::accumulate(m_distance_moved.begin(), m_distance_moved.end(), 0ul, [](uint64_t acc, auto& entry) {
+        return acc + entry.second;
+    });
+}
+
+void PowerStatsListener::on_elevator_stopped(Time, Time duration, ElevatorState const& state)
+{
+    if (!state.passengers().empty())
+        m_time_stopped_with_passengers += duration;
+}
+
+}
---
+++ games/elevated/stats/PowerStatsListener.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Listener.h"
+
+namespace Elevated {
+
+class PowerStatsListener final : public EventListener {
+public:
+    virtual void on_elevator_opened_doors(Time time, ElevatorState const& state) override;
+    virtual void on_elevator_moved(Time time, Height height, ElevatorState const& state) override;
+    virtual void on_elevator_stopped(Time at, Time duration, ElevatorState const& state) override;
+
+    size_t times_door_opened() const { return m_times_door_opened; }
+    size_t total_distance_travelled() const;
+    Time time_stopped_with_passengers() const { return m_time_stopped_with_passengers; }
+
+private:
+    std::unordered_map<Capacity, uint64_t> m_distance_moved;
+    uint64_t m_times_door_opened{0};
+    Time m_time_stopped_with_passengers{0};
+};
+
+}
