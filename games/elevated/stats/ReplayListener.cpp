f9c43978555319b80805efbb934249912767095f
davidot <david.tuin@gmail.com>
2022-05-17 22:47:11 +0200

    Elevated: Add Replay listener for writing events to string
    This will allow us to later replay the events and generate new stats
    or replays.

---
+++ games/elevated/stats/ReplayListener.cpp
@@ -0,0 +1,51 @@
+#include "ReplayListener.h"
+
+namespace Elevated {
+
+void ReplayListener::on_elevator_opened_doors(Time time, const ElevatorState& state)
+{
+    current_stream << time << " opened " << state.id << ' ' << state.height() << '\n';
+}
+
+void ReplayListener::on_elevator_closed_doors(Time time, const ElevatorState& state)
+{
+    current_stream << time << " closed " << state.id << ' ' << state.height() << '\n';
+}
+
+void ReplayListener::on_elevator_stopped(Time at, Time duration, const ElevatorState& state)
+{
+    current_stream << at << " stopped " << state.id << ' ' << duration << '\n';
+}
+
+void ReplayListener::on_elevator_moved(Time time, Height height, Height initial_height, const ElevatorState& state)
+{
+    current_stream << time << " moved " << state.id << ' ' << height << ' ' << initial_height << '\n';
+}
+
+void ReplayListener::on_request_created(Time time, const Passenger& passenger)
+{
+    current_stream << time << " request "
+                   << passenger.from << ' '
+                   << passenger.to << ' '
+                   << passenger.group << ' '
+                   << passenger.capacity << ' '
+                   << passenger.id << '\n';
+}
+
+void ReplayListener::on_passenger_enter_elevator(Time time, const Passenger& passenger, ElevatorID id)
+{
+    current_stream << time << " enter " << passenger.id << ' ' << id << '\n';
+}
+
+void ReplayListener::on_passenger_leave_elevator(Time time, PassengerID id, Height height)
+{
+    current_stream << time << " leave " << id << '\n';
+}
+
+void ReplayListener::on_elevator_set_target(Time time, Height new_target, const ElevatorState& state)
+{
+    current_stream << time << " target " << state.id << ' ' << new_target << ' ' << state.height() << '\n';
+}
+
+}
+
---
+++ games/elevated/stats/ReplayListener.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Listener.h"
+#include <sstream>
+
+namespace Elevated {
+
+class ReplayListener final : public EventListener {
+public:
+    virtual void on_elevator_opened_doors(Time time, ElevatorState const& state) override;
+    virtual void on_elevator_closed_doors(Time time, ElevatorState const& state) override;
+    virtual void on_elevator_stopped(Time at, Time duration, ElevatorState const& state) override;
+    virtual void on_elevator_moved(Time time, Height height, Height initial_height, ElevatorState const& state) override;
+    virtual void on_request_created(Time time, Passenger const& passenger) override;
+    virtual void on_passenger_enter_elevator(Time time, Passenger const& passenger, ElevatorID id) override;
+    virtual void on_passenger_leave_elevator(Time time, PassengerID id, Height height) override;
+    virtual void on_elevator_set_target(Time time, Height new_target, ElevatorState const& state) override;
+
+private:
+    std::ostringstream current_stream;
+};
+
+}
