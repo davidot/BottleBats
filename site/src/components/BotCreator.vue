a75eb1b63e20f4fe8ba7d0ab0f27ad20d49598d1
davidot <david.tuin@gmail.com>
2022-06-12 23:41:47 +0200

    Site: Add icon maker to bot creator

--- site/src/components/BotCreator.vue
+++ site/src/components/BotCreator.vue
@@ -14,7 +14,7 @@
         </div>
         <div class="active-file" v-for="(file, index) of currentFiles" :key="file.name" @click.prevent.stop="currentFiles.splice(index, 1)">
           <img src="@/assets/file.png" style="width: 1em"/>
-          {{ file.name }}❌
+          {{ file.name }} ❌
         </div>
       </div>
       <input
@@ -25,16 +25,7 @@
           :multiple="multiple"
           @change="selectFile"
       />
-      <canvas id="image-convert" width="124" height="124" ref="converter" style="border: black 1px solid; width: 124px; height: 124px;" @click="draw" @drag="draw" @mousemove="draw"/>
-      <div style="width: 50px; height: 50px" :style="{'background-color': currentColor}"></div>
-      <select v-model="currentColor">
-        <option value="red">
-          Rood
-        </option>
-        <option value="blue">
-          Blauw
-        </option>
-      </select>
+      <IconMaker v-if="withImage" ref="botImages"/>
     </div>
     <div>
       <label for="new-bot-name"> Naam: </label>
@@ -44,15 +35,17 @@
         Upload bot
       </button>
     </div>
-    <span style="color: red">{{ lastError }}</span>
+    <span :style="{ 'color': failed ? 'red' : 'gray'}">{{ lastMessage }}</span>
   </div>
 </template>
 
 <script>
 import {endpoint} from "@/http";
+import IconMaker from "@/components/IconMaker.vue";
 
 export default {
   name: "BotCreator",
+  components: {IconMaker},
   props: {
     uploadUrl: String,
     multiple: {
@@ -64,19 +57,12 @@ export default {
       type: Boolean,
     }
   },
-  mounted() {
-    const context = this.$refs.converter.getContext('2d');
-    for (var i = 0; i < 31; i++) {
-      context.fillStyle = `hsl(${i * 60}, 100%, 50%)`;
-      context.fillRect(i * 4, i * 4, 4, 4);
-    }
-  },
   data() {
     return {
       currentFiles: [],
-      currentColor: 'red',
       anyDrawn: false,
-      lastError: "",
+      lastMessage: "",
+      failed: false,
       dropActive: false,
       inActiveTimeout: null,
       botName: "",
@@ -96,27 +82,34 @@ export default {
       const data = new FormData();
       data.append("name", name);
 
-      if (this.anyDrawn) {
-        const imgBlob = await new Promise(resolve => this.$refs.converter.toBlob(resolve, "image/png"));
-        data.append("image", imgBlob);
+      if (this.withImage) {
+        const imgBlob = await this.$refs.botImages.getBlob();
+        if (imgBlob !== null)
+          data.append("image", imgBlob);
       }
 
       let i = 0;
       for (let file of files)
         data.append("src_" + (i++), file);
 
-      endpoint.post(this.uploadUrl, data).then((done) => {
-        console.log('response', done.data);
-        this.anyDrawn = false;
-        this.$refs.converter.getContext('2d').clearRect(0, 0, 128, 128);
-        this.currentFiles = [];
-        this.uploading = false;
-        this.$emit('new-bot');
-      })
-          .catch(({ response }) => {
-            this.uploading = false;
-            this.lastError = response.data;
-          });
+      endpoint
+        .post(this.uploadUrl, data)
+        .then((done) => {
+          this.lastMessage = done.data;
+          this.failed = false;
+
+          if (this.withImage)
+            this.$refs.botImages.clear();
+
+          this.currentFiles = [];
+          this.uploading = false;
+          this.$emit("new-bot");
+        })
+        .catch(({ response }) => {
+          this.uploading = false;
+          this.lastMessage = response.data;
+          this.failed = true;
+        });
     },
     addFile(file) {
       if (!file)
@@ -124,46 +117,35 @@ export default {
 
       let maxSize = 2500000;
       if (file.size > maxSize) {
-        this.lastError = `File is een beetje te groot, max ${
+        this.lastMessage = `File is een beetje te groot, max ${
             maxSize / 1000
         }kb aub`;
         return;
       }
 
-      // if (
-      //   file.name.endsWith(".png") ||
-      //   file.name.endsWith(".jpg") ||
-      //   file.name.endsWith(".jpeg")
-      // ) {
-      //   console.log('converting!');
-      //   const context = this.$refs.converter.getContext('2d');
-      //   const img = new Image();
-      //   img.onload = () => {
-      //     context.drawImage(img, 0, 0, 128, 128);
-      //     this.$refs.converter.toBlob(async (blob) => {
-      //
-      //       console.log('blob', await blob.text());
-      //       }, "image/png");
-      //     console.log(png);
-      //     console.log(data);
-      //   };
-      //   img.src = URL.createObjectURL(file);
-      //   return;
-      // }
+      if (
+        file.type.startsWith("image/")
+      ) {
+        const img = new Image();
+        img.onload = () => {
+          this.$refs.botImages.drawImage(img);
+        };
+        img.src = URL.createObjectURL(file);
+        return;
+      }
 
       if (!this.multiple) {
         this.currentFiles = [file];
       } else {
         this.currentFiles.push(file);
       }
-
     },
     selectFile() {
       const files = this.$refs.fileInput.files;
-      this.lastError = '';
+      this.lastMessage = "";
 
       if (!this.multiple && files.length !== 1) {
-        this.lastError = "Precies 1 file graag, niet " + files.length;
+        this.lastMessage = "Precies 1 file graag, niet " + files.length;
         return;
       }
 
@@ -174,17 +156,17 @@ export default {
     onDrop(event) {
       const data = event.dataTransfer;
       this.dropActive = false;
-      this.lastError = '';
+      this.lastMessage = "";
 
       if (!this.multiple && data.items.length > 1) {
-        this.lastError = "Precies 1 file graag, niet " + data.items.length;
+        this.lastMessage = "Precies 1 file graag, niet " + data.items.length;
         return;
       }
 
       for (let item of data.items) {
 
         if (item.kind !== "file") {
-          this.lastError = "Een file graag, niet " + item.kind;
+          this.lastMessage = "Een file graag, niet " + item.kind;
           return;
         }
 
@@ -198,11 +180,16 @@ export default {
     setInactive() {
       this.inActiveTimeout = setTimeout(() => (this.dropActive = false), 50);
     },
-    draw(event) {
-      const context = this.$refs.converter.getContext('2d');
-      context.fillStyle = this.currentColor;
-      context.fillRect(4 * Math.floor(event.offsetX / 4), 4 * Math.floor(event.offsetY / 4), 4, 4);
-      this.anyDrawn = true;
+    useImage(e) {
+      const src = e?.target?.src;
+      if (!src)
+        return;
+      const img = new Image();
+
+      img.onload = () => {
+        this.$refs.botImages.drawImage(img);
+      };
+      img.src = src;
     }
   }
 };
---
+++ site/src/components/IconMaker.vue
@@ -0,0 +1,202 @@
+<template>
+  <div>
+    <canvas id="image-convert" :width="width" :height="height" ref="canvas" style="padding: 5px; border: black 3px dashed; width: 124px; height: 124px;"
+            @mousedown="draw" @mouseup="draw" @mousemove="draw" @mouseleave="draw" @mouseenter="draw"
+            @drop.prevent="drop"
+            @dragenter.prevent=""
+            @dragover.prevent=""/>
+    <input type="color" @change="updateColor" style="display: none" ref="colorPicker"/>
+    <div style="display: flex; flex-direction: row">
+      <div style="margin-left: auto; margin-right: auto; width: 80%; height: 20px" :style="{'background-color': currentColor}" @click="$refs.colorPicker.click">
+      </div>
+      <button @click="clear">
+        ❌
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IconMaker",
+  mounted() {
+    this.context2D = this.$refs.canvas.getContext("2d");
+  },
+  data() {
+    return {
+      width: 4 * 31,
+      height: 4 * 31,
+      currentColor: "#000000",
+      context2D: null,
+      anyDrawn: false,
+      pressed: false,
+      location: {
+        x: -1,
+        y: -1,
+      },
+    };
+  },
+  methods: {
+    drawImage(img) {
+      this.context2D.drawImage(img, 0, 0, this.width, this.height);
+    },
+    draw(event) {
+      this.location.x = event.offsetX;
+      this.location.y = event.offsetY;
+
+      if (event.type === "mouseup" || event.type === "mouseleave") {
+        this.pressed = false;
+        return;
+      }
+
+      if (event.type === "mouseenter") {
+        if ((event.buttons & 1) === 1)
+          this.pressed = true;
+        return;
+      }
+
+      this.context2D.fillStyle = this.currentColor;
+      if (event.type === "mousedown") {
+        this.pressed = true;
+        this.anyDrawn = true;
+        if (event.shiftKey) {
+          this.context2D.fillRect(0, 0, this.width, this.height);
+          return;
+        }
+        if (event.ctrlKey) {
+
+          const red = parseInt(this.currentColor.slice(1, 3), 16);
+          const green = parseInt(this.currentColor.slice(3, 5), 16);
+          const blue = parseInt(this.currentColor.slice(5, 7), 16);
+          let alpha = 255;
+          if (this.currentColor.length > 7) {
+            alpha = parseInt(this.currentColor.slice(7, 9), 16);
+          }
+
+          const uintColor = (alpha << 24) | (blue << 16) | (green << 8) | red;
+
+          this.floodFill(
+            this.context2D,
+            this.location.x + 0,
+            this.location.y + 0,
+            uintColor
+          );
+          return;
+        }
+
+        this.context2D.fillRect(
+          4 * Math.floor(this.location.x / 4),
+          4 * Math.floor(this.location.y / 4),
+          4,
+          4
+        );
+      } else if (event.type === "mousemove" || event.type === "drag") {
+        if (!this.pressed)
+            return;
+
+        this.anyDrawn = true;
+        const start = [
+          event.offsetX - event.movementX,
+          event.offsetY - event.movementY,
+        ];
+        const step = [event.movementX, event.movementY];
+
+        for (let i = 0; i < 1.0; i += 0.02) {
+          const x = start[0] + i * step[0];
+          const y = start[1] + i * step[1];
+
+          this.context2D.fillRect(
+              4 * Math.floor(x / 4),
+              4 * Math.floor(y / 4),
+              4,
+              4
+          );
+        }
+
+      }
+    },
+    getBlob() {
+      if (!this.anyDrawn)
+        return null;
+
+        return new Promise(resolve => this.$refs.canvas.toBlob(resolve, "image/png"));
+    },
+    clear() {
+      this.context2D.clearRect(0, 0, 128, 128);
+      this.anyDrawn = false;
+    },
+    updateColor(event) {
+      this.currentColor = this.$refs.colorPicker.value;
+    },
+    async floodFill(ctx, x, y, fillColor) {
+      function wait(delay = 1) {
+        return new Promise((resolve) => {
+          setTimeout(resolve, delay);
+        });
+      }
+
+      function getPixel(pixelData, x, y) {
+        if (x < 0 || y < 0 || x >= pixelData.width || y >= pixelData.height) {
+          return -1;
+        } else {
+          return pixelData.data[y * pixelData.width + x];
+        }
+      }
+
+
+      // read the pixels in the canvas
+      const imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+      // make a Uint32Array view on the pixels so we can manipulate pixels
+      // one 32bit value at a time instead of as 4 bytes per pixel
+      const pixelData = {
+        width: imageData.width,
+        height: imageData.height,
+        data: new Uint32Array(imageData.data.buffer),
+      };
+
+      // get the color we're filling
+      const targetColor = getPixel(pixelData, x, y);
+      console.log('target color', targetColor, fillColor);
+
+      let waits = 0;
+
+      // check we are actually filling a different color
+      if (targetColor !== fillColor) {
+
+        const ticksPerUpdate = 80;
+        let tickCount = 0;
+        const pixelsToCheck = [x, y];
+        while (pixelsToCheck.length > 0) {
+          const y = pixelsToCheck.pop();
+          const x = pixelsToCheck.pop();
+
+          const currentColor = getPixel(pixelData, x, y);
+          if (currentColor === targetColor) {
+            pixelData.data[y * pixelData.width + x] = fillColor;
+
+            // put the data back
+            ctx.putImageData(imageData, 0, 0);
+            ++tickCount;
+            if (tickCount % ticksPerUpdate === 0) {
+              waits++;
+              if (waits >= 600)
+                return;
+              await wait();
+            }
+
+            pixelsToCheck.push(x + 1, y);
+            pixelsToCheck.push(x - 1, y);
+            pixelsToCheck.push(x, y + 1);
+            pixelsToCheck.push(x, y - 1);
+          }
+        }
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
