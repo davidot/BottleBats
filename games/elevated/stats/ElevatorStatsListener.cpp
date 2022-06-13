c792fe3ddb6a7cd1f64a5745d954b758a5c9d9be
davidot <david.tuin@gmail.com>
2022-06-13 15:25:43 +0200

    Elevated: Remove unused parameters

--- games/elevated/stats/ElevatorStatsListener.cpp
+++ games/elevated/stats/ElevatorStatsListener.cpp
@@ -2,17 +2,17 @@
 
 namespace Elevated {
 
-void ElevatorStatsListener::on_elevator_opened_doors(Time time, const ElevatorState& state)
+void ElevatorStatsListener::on_elevator_opened_doors(Time, const ElevatorState& state)
 {
     m_times_doors_opened.add_to_observation(state.id, 1);
 }
 
-void ElevatorStatsListener::on_elevator_moved(Time time, Height distance_travelled, Height before_height, const ElevatorState& state)
+void ElevatorStatsListener::on_elevator_moved(Time, Height distance_travelled, Height, const ElevatorState& state)
 {
     m_distance_travelled.add_to_observation(state.id, distance_travelled);
 }
 
-void ElevatorStatsListener::on_elevator_stopped(Time at, Time duration, const ElevatorState& state)
+void ElevatorStatsListener::on_elevator_stopped(Time, Time duration, const ElevatorState& state)
 {
     m_time_stopped.add_to_observation(state.id, duration);
 }
