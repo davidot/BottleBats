00c4d30e66534bdafd77ddd20573de9565f8a7cf
davidot <david.tuin@gmail.com>
2024-08-31 01:39:42 +0200

    Add tick tack toe implementation

--- server/full-server.cpp
+++ server/full-server.cpp
@@ -1,5 +1,6 @@
 #include "crow/app.h"
 #include "crow/common.h"
+#include "crow/http_response.h"
 #include "crow/json.h"
 #include "crow/returnable.h"
 #include "crow/websocket.h"
@@ -7,6 +8,7 @@
 #include <atomic>
 #include <cstdint>
 #include <string_view>
+#include <tuple>
 #include <utility>
 #include <variant>
 #include <optional>
@@ -330,6 +332,207 @@ private:
         "random",
     };
 };
+
+class TTTPlayer {
+public:
+    enum class FieldValue {
+        Empty,
+        Cross,
+        Circle
+    };
+
+    static char to_char(FieldValue value) {
+        switch (value) {
+            case FieldValue::Empty:
+                return '-';
+            case FieldValue::Cross:
+                return 'X';
+            case FieldValue::Circle:
+                return 'O';
+        }
+        ASSERT(false);
+    }
+
+    virtual std::optional<size_t> play(std::array<FieldValue, 9> const& field, FieldValue you) = 0;
+    virtual ~TTTPlayer() {}
+};
+
+
+class InteractiveTTTPlayer: public TTTPlayer {
+public:
+    InteractiveTTTPlayer(std::string& input, std::vector<std::string>& output) :
+        m_input_buffer(input), m_output_buffer(output) {}
+
+    std::optional<size_t> play(std::array<FieldValue, 9> const& field, FieldValue you) override {
+        if (!sent_output) {
+            sent_output = true;
+            std::ostringstream message {};
+            ASSERT(you == FieldValue::Circle || you == FieldValue::Cross);
+            message << "turn " << to_char(you) << ' ';
+            for (size_t i = 0; i < field.size(); ++i) {
+                if (i > 0 && i % 3 == 0)
+                    message << '|';
+                message << to_char(field[i]);
+            }
+            m_output_buffer.emplace_back(message.str());
+        }
+
+        auto end_of_line = m_input_buffer.find('\n');
+        if (end_of_line == std::string::npos)
+            return std::nullopt;
+
+        int value = -1;
+        auto result = std::from_chars(m_input_buffer.data(), m_input_buffer.data() + end_of_line, value);
+        if (result.ec != std::errc{})
+            return std::nullopt; // INVALID!! STOP HERE!
+
+        if (value < 0 || value > 8)
+            return std::nullopt; // INVALID!! STOP HERE!
+
+        sent_output = false;
+        m_input_buffer.erase(0, end_of_line + 1);
+
+        return value;
+    }
+
+private:
+    bool sent_output = false;
+    std::string& m_input_buffer;
+    std::vector<std::string>& m_output_buffer;
+};
+
+class FirstSpotPlayer : public TTTPlayer {
+    std::optional<size_t> play(std::array<FieldValue, 9> const& field, FieldValue) override {
+        for (size_t i = 0; i < field.size(); ++i) {
+            if (field[i] == FieldValue::Empty)
+                return i;
+        }
+        ASSERT(false);
+    }
+};
+
+
+struct TTTGameState : public InteractiveGameState {
+    using FieldValue = TTTPlayer::FieldValue;
+
+    PlayerIdentifier turnForPlayer = 0;
+    std::array<FieldValue, 2> player_mapping{};
+    std::array<FieldValue, 9> field{};
+
+    std::array<std::unique_ptr<TTTPlayer>, 2> players{};
+
+    explicit TTTGameState(std::array<std::unique_ptr<TTTPlayer>, 2> new_players, FieldValue starting_player)
+        : player_mapping({starting_player, invert(starting_player)}), players(std::move(new_players))
+    {
+        field.fill(FieldValue::Empty);
+    }
+
+    static FieldValue invert(FieldValue value) {
+        switch(value) {
+            case FieldValue::Circle:
+                return FieldValue::Cross;
+            case FieldValue::Cross:
+                return FieldValue::Circle;
+            case FieldValue::Empty:
+                return FieldValue::Empty;
+        }
+        ASSERT(false);
+    }
+
+    bool win_for(FieldValue symbol) const {
+        ASSERT(symbol == FieldValue::Cross || symbol == FieldValue::Circle);
+        ASSERT(!triple_for(invert(symbol)));
+        return triple_for(symbol);
+    }
+private:
+
+    static std::array<std::tuple<uint8_t, uint8_t, uint8_t>, 8> constexpr lines = {{
+        {0, 1, 2},
+        {3, 4, 5},
+        {6, 7, 8},
+
+        {0, 3, 6},
+        {1, 4, 7},
+        {2, 5, 8},
+
+        {0, 4, 8},
+        {2, 4, 6}
+    }};
+
+    bool triple_for(FieldValue symbol) const {
+        for (auto& [a, b, c] : lines) {
+            if (field[a] == symbol && field[b] == symbol && field[c] == symbol)
+                return true;
+        }
+        return false;
+    }
+
+};
+
+
+struct TTTGame final : public MultiplayerGame<TTTGameState> {
+    virtual uint32_t get_num_players() const override {
+        return 2;
+    };
+
+    virtual std::vector<std::string> const& available_algortihms() const override {
+        return m_algorithms;
+    };
+
+
+    TTTGameState* game_from_commands(std::vector<PlayerFactory> const& setup) const override
+    {
+        ASSERT(setup.size() == 2);
+        std::array<std::unique_ptr<TTTPlayer>, 2> players;
+        for (size_t i = 0; i < 2; ++i) {
+            auto& command = setup[i];
+            if (command.is_interactive()) {
+                players[i] = command.construct_interactive<InteractiveTTTPlayer>();
+            } else {
+                players[i] = std::make_unique<FirstSpotPlayer>();
+            }
+        }
+
+        return new TTTGameState {
+            std::move(players), TTTGameState::FieldValue::Cross
+        };
+    }
+
+    std::optional<PlayerIdentifier> tick_game_state(TTTGameState& game_state) const override
+    {
+
+        while (true) {
+            PlayerIdentifier this_player = game_state.turnForPlayer;
+            auto& player = game_state.players[game_state.turnForPlayer];
+            TTTGameState::FieldValue player_symbol = game_state.player_mapping[this_player];
+            auto potential_guess = player->play(game_state.field, player_symbol);
+            if (!potential_guess.has_value()) {
+                std::cout << "Player " << this_player << " has no move ready waiting...\n";
+                return this_player;
+            }
+
+            size_t guess = *potential_guess;
+            game_state.field[guess] = player_symbol;
+
+            if (game_state.win_for(player_symbol)) {
+                break;
+            }
+
+            game_state.turnForPlayer = (game_state.turnForPlayer + 1) % game_state.players.size();
+        }
+
+        return {};
+    };
+
+private:
+
+    std::vector<std::string> m_algorithms {
+        "random",
+        "first-spot",
+    };
+
+};
+
 
 struct InteractiveGameSetup {
     std::optional<PlayerIdentifier> player_id;
@@ -915,6 +1118,7 @@ int main()
     srand(time(nullptr));
 
     games.emplace_back("guess", "Guess", std::make_unique<GuessGame>(5));
+    games.emplace_back("ttt", "TickTackToe", std::make_unique<TTTGame>());
     random_engine.seed(rand());
 
     crow::SimpleApp app;
