1cbf3df77aa758405b08d2ba48cb202164289067
davidot <david.tuin@gmail.com>
2024-12-28 12:16:31 +0100

    Add Blokus link for downloading json

---
+++ site/src/components/blokus/BlokusPieceLink.vue
@@ -0,0 +1,11 @@
+<template>
+    <a :href="downloadURL" download="pieces.json">
+        <slot></slot>
+    </a>
+</template>
+
+<script setup>
+import rawPieces from '@/components/blokus/pieces.json';
+
+const downloadURL = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(rawPieces, null, 4));
+</script>
